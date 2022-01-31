f104ff8e462559e2c56cf3f50651135e61663c6f
Robert Albenda <ralbenda@gmail.com>
2022-01-30 20:19:32 -0500

    second hw update

--- server.js
+++ server.js
@@ -1,52 +1,93 @@
-const http = require('http');
 
-const server = http.createServer((req, res) => {
-    if(req.url === '/') {
-        res.write(`
-        <html>
+const express = require('express');
+
+const app = express();
+
+app.get('/',(req,res,next) => {
+    res.send(
+       ` <html>
             <body>
+            <nav>
+                <a href='/'>Home</a>
+                <a href='/aboutEddie'> Biography</a>
+                <a href='/discography'>Discography</a>
+                <a href='/testimonials'>Testimonials</a>
+            </nav>
                 <h1> Eddie Van Halen</h1>
             </body>
-        </html>
-        `)
-    }
+        </html>`
 
-    if(req.url === '/aboutEddie') {
-        res.write(`
-        <html>
-            <body>
-                <h1> A Little Bit About Eddie!</h1>
-            </body>
-        </html>
-        `)
-    }
+    );
 
-    if(req.url === '/discography') {
-        res.write (`
-        <html>
-            <body>
-                <h1> Eddie Van Halen Discography</h1>
-                <ul>
-                    <li> Van Halen </li>
-                    <li> Van Halen II </li>
-                    <li> Women and Children First </li>
-                    <li> Fair Warning </li>
-                    <li> Diver Down </li>
-                    <li> 1984 </li>
-                    <li> 5150 </li>
-                    <li> OU812 </li>
-                    <li> For Unlawful Carnal knowledge </li>
+})
 
-                </ul>
-            </body>
-        </html>    
-        `)
-     }
+app.get('/aboutEddie',(req,res,next) => {
+    res.send(
+       `<html>
+       <body>
+       <nav>
+           <a href='/'>Home</a>
+           <a href='/aboutEddie'> Biography</a>
+           <a href='/discography'>Discography</a>
+           <a href='/testimonials'>Testimonials</a>
+       </nav>
+           <h1> A Little Bit About Eddie!</h1>
+       </body>
+   </html> `
 
-        res.end();
-    
+    );
+
+})
+
+app.get('/discography',(req,res,next) => {
+    res.send(
+       `<html>
+       <body>
+           <nav>
+               <a href='/'>Home</a>
+               <a href='/aboutEddie'> Biography</a>
+               <a href='/discography'>Discography</a>
+               <a href='/testimonials'>Testimonials</a>
+           </nav>
+           <h1> Eddie Van Halen Discography</h1>
+           <ul>
+               <li> Van Halen </li>
+               <li> Van Halen II </li>
+               <li> Women and Children First </li>
+               <li> Fair Warning </li>
+               <li> Diver Down </li>
+               <li> 1984 </li>
+               <li> 5150 </li>
+               <li> OU812 </li>
+               <li> For Unlawful Carnal knowledge </li>
+
+           </ul>
+       </body>
+   </html>   `
+
+    );
+
+})
+
+app.get('/testimonials',(req,res,next) => {
+    res.send(
+       `<html>
+       <body>
+       <nav>
+           <a href='/'>Home</a>
+           <a href='/aboutEddie'> Biography</a>
+           <a href='/discography'>Discography</a>
+           <a href='/testimonials'>Testimonials</a>
+       </nav>
+           <h1> Testimonials </h1>
+       </body>
+   </html>
+`
+
+    );
+
 })
 
 const port = process.env.PORT || 3000;
 
-server.listen(port, (console.log(`listening on port ${port}`)))
+app.listen(port, () => console.log(`listening on port ${port}`))
